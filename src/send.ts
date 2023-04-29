b78b381b8a9017d442eef21f589e155ea9858bcc
hkgnp <github.com/hkgnp>
2023-04-29 12:35:57 +0800

    [chore] refactor

--- src/send.ts
+++ src/send.ts
@@ -1,12 +1,13 @@
-export default async function send(block) {
+export default async function send(block: { uuid: string }) {
   const blk = await logseq.Editor.getBlock(block.uuid);
   try {
     const currGraph = await logseq.App.getCurrentGraph();
-
     window.open(
       `things:///add?title=${encodeURIComponent(
         blk!.content
-      )}&notes=logseq://graph/${currGraph!.name}?block-id=${blk?.uuid}`
+      )}&notes=logseq://graph/${currGraph!.name}?block-id=${
+        blk?.uuid
+      }&x-success`
     );
 
     logseq.UI.showMsg("Successfully sent to Things!", "success");
